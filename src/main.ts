6c55981e6fd8fd5ca94a2998c917725c240d8fb1
Camille-Bonin-Di-Sera <camille.bonin@le-campus-numerique.fr>
2023-02-08 11:44:03 +0100

    dossier js

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from  'vue'
+import App from  './App.vue'
+import router from  './router'
+import  './index.css'
+import axios from  'axios'
 
-import './assets/main.css'
+axios.defaults.baseURL = 'http://localhost:8000/api/v1/' ;
+
+import './assets/Style/main.css'
 
 const app = createApp(App)
 
+// @ts-ignore
 app.use(router)
 
 app.mount('#app')
