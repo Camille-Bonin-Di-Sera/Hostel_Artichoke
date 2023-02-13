829794fc6e5abe550d45b84511c60855001d7afa
Mathieu <berthetmathieu@free.fr>
2023-02-13 16:30:39 +0100

    login

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -11,20 +11,20 @@
       <article class="sm:w-1/2">
         <h3 class="text-green-700 text-center text-xl sm:text-3xl"> Créer un compte </h3>
         <form class="space-x-4 space-y-4">
-          <input type="text" placeholder="Prénom" class="border border-black ml-4 w-1/2">
-          <input type="text" placeholder="Nom" class="border border-black w-1/3">
+          <input type="text" placeholder=" Prénom" class="border border-black ml-4 w-1/2">
+          <input type="text" placeholder=" Nom" class="border border-black w-1/3">
           <br />
-          <input type="text" placeholder="Pseudo" class="border border-black">
+          <input type="text" placeholder=" Pseudo" class="border border-black">
           <br />
-          <input type="email" placeholder="E-mail" class="border border-black">
+          <input type="email" placeholder=" E-mail" class="border border-black">
           <br />
-          <input type="text" placeholder="Telephone" class="border border-black">
+          <input type="text" placeholder=" Telephone" class="border border-black">
           <br />
-          <input type="password" placeholder="Mot de passe" class="border border-black w-1/2">
-          <input type="password" placeholder="Mot de passe" class="border border-black w-1/3">
+          <input type="password" placeholder=" Mot de passe" class="border border-black w-1/2">
+          <input type="password" placeholder=" Mot de passe" class="border border-black w-1/3">
           <br />
           <div>
-            <button type="submit" class="text-white pr-2 pl-2 text-lg bg-green-700 block m-auto sm:w-1/2 rounded sm:text-2xl"> Creer mon compte </button>
+            <button type="submit" class="text-white pr-2 pl-2 text-lg bg-green-700 block m-auto sm:w-1/2 rounded sm:text-2xl"> Créer mon compte </button>
           </div>
         </form>
         <br />
@@ -33,9 +33,9 @@
       <article class="sm:float-left sm:w-1/2 ">
         <h3 class="text-green-700 text-center text-xl sm:text-3xl"> Me connecter </h3>
         <form class="space-x-4 space-y-4 md:items-center md:w-3/4 md:ml-44">
-          <input type="email" placeholder="E-mail" class="border border-black ml-4">
+          <input type="email" placeholder=" E-mail" class="border border-black ml-4">
           <br />
-          <input type="password" placeholder="Mot de passe" class="border border-black">
+          <input type="password" placeholder=" Mot de passe" class="border border-black">
           <br />
           <input type="checkbox"> <label class="text-green-600">Rester connecté</label>
           <br />
