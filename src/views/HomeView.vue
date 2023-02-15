d9bea6600e0047912cda080a0199b55d8d73dd21
Camille-Bonin-Di-Sera <camille.bonin@le-campus-numerique.fr>
2023-02-15 11:25:36 +0100

    delete files caroussel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-import Carousel from '../components/Carousel.vue'
 import HomeComponent from "@/components/HomeComponent.vue";
 
 </script>
 
 <template>
   <main>
-    <Carousel/>
     <HomeComponent />
   </main>
 </template>
