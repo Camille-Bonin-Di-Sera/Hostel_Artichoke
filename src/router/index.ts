81cf169cd5777324c2bcf2b7f0a5458fb7957b36
Camille-Bonin-Di-Sera <camille.bonin@le-campus-numerique.fr>
2023-02-15 11:22:21 +0100

    test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import 'tw-elements';
-import Slider from "@/components/Carousel.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +9,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-
-    {
-      path: '/slider',
-      name: 'Slider',
-      component: Slider
     },
 
     {
