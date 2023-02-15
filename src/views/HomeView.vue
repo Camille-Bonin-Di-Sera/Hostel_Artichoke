a436c05c7fea5463e57c770f48b4400e25f9fe58
Camille-Bonin-Di-Sera <camille.bonin@le-campus-numerique.fr>
2023-02-15 13:17:11 +0100

    add carousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-
+import Carousel from "@/components/Carousel.vue";
 import HomeComponent from "@/components/HomeComponent.vue";
 
 </script>
 
 <template>
   <main>
+    <Carousel/>
     <HomeComponent />
   </main>
 </template>
