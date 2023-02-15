3d8469eeeebc531d1bd00e7e266fd94691918f1d
Mathieu <berthetmathieu@free.fr>
2023-02-15 11:45:19 +0100

    conflict in my branch

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <AboutComponent/>
   </div>
 </template>
 
@@ -15,5 +14,5 @@
 }
 </style>
 <script setup lang="ts">
-import AboutComponent from "@/components/AboutComponent.vue";
+
 </script>
