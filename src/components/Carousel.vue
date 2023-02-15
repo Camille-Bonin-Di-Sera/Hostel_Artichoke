59525f41722cbe1febb29da03fa8ccaa1a0eb3cd
Camille-Bonin-Di-Sera <camille.bonin@le-campus-numerique.fr>
2023-02-15 11:04:00 +0100

    delete carousel

--- src/components/Carousel.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="content">
-    <img class="bed" src="../assets/Pictures/bed-section-hero.jpg">
-  </div>
-</template>
-<script>
-import("../assets/Style/Slider.css")
-</script>
