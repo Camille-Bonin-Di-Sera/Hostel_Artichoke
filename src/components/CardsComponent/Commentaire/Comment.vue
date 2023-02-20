5c0dd80aa368a1fca668a8e97aa2161b5f978591
Camille-Bonin-Di-Sera <camille.bonin@le-campus-numerique.fr>
2023-02-20 13:26:02 +0100

    touch responsive

---
+++ src/components/CardsComponent/Commentaire/Comment.vue
@@ -0,0 +1,70 @@
+<template>
+
+    <h2  class="text-5xl text-center md:text-6xl text-green mt-12">Votre avis compte</h2>
+    <article class="flex flex-row justify-center gap-3">
+      <aside class=" Comment_card mt-12 bg-green ">
+        <aside class="picture_comment flex justify-center">
+          <img
+              src="../../../assets/Pictures/harold.jpg"
+              class="avatar_comment border rounded-full"
+              alt="Avatar"
+          />
+        </aside>
+
+
+        <aside class="comment_text">
+          <div class="flex flex-row mt-4 ">
+            <h3 class="text-xl text-white ml-3 mb-2">Harold</h3>
+            <p class="text-left ml-40">Note : mettre API</p>
+          </div>
+          <p class="text-white md:mt-4">It's not a potato it's my son. Oooh is not a potatoooOOOoooOOO is my son</p>
+          <p class="text-left ml-4 mt-6 mb-2 italic">Posté le 15/02/2023</p>
+        </aside>
+      </aside>
+
+      <aside class=" Comment_card mt-12 bg-green ">
+        <aside class="picture_comment flex justify-center">
+          <img
+              src="../../../assets/Pictures/harold.jpg"
+              class="avatar_comment border rounded-full"
+              alt="Avatar"
+          />
+        </aside>
+        <aside class="comment_text">
+          <div class="flex flex-row mt-4 ">
+            <h3 class="text-xl text-white ml-3 mb-2">Harold</h3>
+            <p class="text-left ml-40">Note : mettre API</p>
+          </div>
+          <p class="text-white md:mt-4">It's not a potato it's my son. Oooh is not a potatoooOOOoooOOO is my son</p>
+          <p class="text-left ml-4 mt-6 mb-2 italic">Posté le 15/02/2023</p>
+        </aside>
+      </aside>
+
+
+
+      <aside class=" Comment_card mt-12 bg-green ">
+        <aside class="picture_comment flex justify-center">
+          <img
+              src="../../../assets/Pictures/harold.jpg"
+              class="avatar_comment border rounded-full"
+              alt="Avatar"
+          />
+        </aside>
+        <aside class="comment_text">
+          <div class="flex flex-row mt-4 ">
+            <h3 class="text-xl text-white ml-3 mb-2">Harold</h3>
+            <p class="text-left ml-40">Note : mettre API</p>
+          </div>
+          <p class="text-white md:mt-4">It's not a potato it's my son. Oooh is not a potatoooOOOoooOOO is my son</p>
+          <p class="text-left ml-4 mt-6 mb-2 italic">Posté le 15/02/2023</p>
+        </aside>
+      </aside>
+
+
+    </article>
+
+</template>
+<script>
+
+import('../../../assets/Style/Cards/Comment.css')
+</script>
--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -177,72 +177,10 @@
       </div>
       <CardActu/>
       </section>
-
       <section class="Three_comment hidden md:block md:border-t-2 md:border-green md:mb-80">
-        <h2  class="text-5xl	text-center md:text-6xl text-green mt-12">Votre avis compte</h2>
-       <article class="flex flex-row justify-center gap-3">
-         <aside class=" Comment_card mt-12 bg-green ">
-           <aside class="picture_comment flex justify-center">
-             <img
-                 src="../assets/Pictures/harold.jpg"
-                 class="avatar_comment border rounded-full"
-                 alt="Avatar"
-             />
-           </aside>
-
-
-          <aside class="comment_text">
-            <div class="flex flex-row mt-4 ">
-              <h3 class="text-xl text-white ml-3 mb-2">Harold</h3>
-              <p class="text-left ml-40">Note : mettre API</p>
-            </div>
-            <p class="text-white md:mt-4">It's not a potato it's my son. Oooh is not a potatoooOOOoooOOO is my son</p>
-            <p class="text-left ml-4 mt-6 mb-2 italic">Posté le 15/02/2023</p>
-          </aside>
-         </aside>
-
-         <aside class=" Comment_card mt-12 bg-green ">
-           <aside class="picture_comment flex justify-center">
-             <img
-                 src="../assets/Pictures/harold.jpg"
-                 class="avatar_comment border rounded-full"
-                 alt="Avatar"
-             />
-           </aside>
-           <aside class="comment_text">
-             <div class="flex flex-row mt-4 ">
-               <h3 class="text-xl text-white ml-3 mb-2">Harold</h3>
-               <p class="text-left ml-40">Note : mettre API</p>
-             </div>
-             <p class="text-white md:mt-4">It's not a potato it's my son. Oooh is not a potatoooOOOoooOOO is my son</p>
-             <p class="text-left ml-4 mt-6 mb-2 italic">Posté le 15/02/2023</p>
-           </aside>
-         </aside>
-
-
-
-         <aside class=" Comment_card mt-12 bg-green ">
-           <aside class="picture_comment flex justify-center">
-             <img
-                 src="../assets/Pictures/harold.jpg"
-                 class="avatar_comment border rounded-full"
-                 alt="Avatar"
-             />
-           </aside>
-           <aside class="comment_text">
-             <div class="flex flex-row mt-4 ">
-               <h3 class="text-xl text-white ml-3 mb-2">Harold</h3>
-               <p class="text-left ml-40">Note : mettre API</p>
-             </div>
-             <p class="text-white md:mt-4">It's not a potato it's my son. Oooh is not a potatoooOOOoooOOO is my son</p>
-             <p class="text-left ml-4 mt-6 mb-2 italic">Posté le 15/02/2023</p>
-           </aside>
-         </aside>
-
-
-       </article>
-
+        <Comment/>
       </section>
+
 
 
 
@@ -256,10 +194,11 @@
 
 import CardPromo from "@/components/CardsComponent/Promotion/CardPromo.vue";
 import CardActu from "@/components/CardsComponent/Actualite/CardActu.vue";
+import Comment from "@/components/CardsComponent/Commentaire/Comment.vue";
 
 import('../assets/Style/Home.css')
 export default {
-  components: {CardActu, CardPromo}
+  components: {Comment, CardActu, CardPromo}
 
 }
 </script>
