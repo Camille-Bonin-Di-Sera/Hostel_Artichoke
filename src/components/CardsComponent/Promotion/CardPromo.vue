807cf63e42ccc86df42de2008eebb0809d16d727
erik <eh@eh-hp-pavilion.lan>
2023-02-21 18:16:34 +0100

    test

--- src/components/CardsComponent/Promotion/CardPromo.vue
+++ src/components/CardsComponent/Promotion/CardPromo.vue
@@ -1,14 +1,13 @@
 <template>
   <section class="Promo mb-16 md:flex md:flex-col ">
     <div class="md:flex-row md:flex md:gap-2 md:h-96">
-      <card-promo-mobile/>
       <CardPromoDesktop/>
-      <CardPromoDesktop/>
+      <CardPromoMobile/>
     </div>
   </section>
 </template>
 <script setup lang="ts">
-  import('../../../assets/Style/Cards/CardsPromo.css');
   import CardPromoDesktop from "../Promotion/CardPromoDesktop.vue";
   import CardPromoMobile from "../Promotion/CardPromoMobile.vue";
+  import('../../../assets/Style/Cards/CardsPromo.css');
 </script>
