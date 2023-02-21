becbf87d6b0d8b12fe746f6e60d31cec6294373e
erik <eh@eh-hp-pavilion.lan>
2023-02-21 18:32:05 +0100

    bug d'import resolu deploiement ok

--- src/components/MobileNavbarBurgerComponent.vue
+++ src/components/MobileNavbarBurgerComponent.vue
@@ -3,7 +3,7 @@
     <a class="inline md:text-2xl" href="/">+ 4 66 99 99 99</a>
     <div class="picture">
       <img src="../assets/Pictures/icon/flag-fr.png" alt="" class="flag_white inline md:mr-6 md:h-12">
-      <img src="../assets/Pictures/icon/flag-eng.png" alt="" class="fflag_white inline md:mr-2 md:h-12">
+      <img src="../assets/Pictures/icon/flag-eng.png" alt="" class="flag_white inline md:mr-2 md:h-12">
     </div>
   </div>
 
