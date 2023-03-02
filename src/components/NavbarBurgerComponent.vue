a75d26ae40cb59621edcbac855d8d750d33fb784
Mathieu <berthetmathieu@free.fr>
2023-03-02 09:37:34 +0100

    resolve button

--- src/components/NavbarBurgerComponent.vue
+++ src/components/NavbarBurgerComponent.vue
@@ -9,9 +9,9 @@
       <a class="md:hover:border-b-2 md:hover:border-green md:text-2xl" href="/">{{ this.lang_fr ? "Réserver" : "Book" }}</a>
 
       <div>
-        <button v-on:click="$emit('changeFR')"><img src="../assets/Pictures/icon/flag-fr.png" alt="" class="inline md:mr-6 md:h-12"></button>
-        <button v-on:click="$emit('changeFR')"><img src="../assets/Pictures/icon/flag-eng.png" alt="" class="inline md:mr-2 md:h-12"></button>
-    </div>
+        <button v-on:click="$emit('changeFR')" class="languageButton"><img src="../assets/Pictures/icon/flag-fr.png" alt="" class="inline md:mr-6 md:h-12"></button>
+        <button v-on:click="$emit('changeFR')" class="languageButton"><img src="../assets/Pictures/icon/flag-eng.png" alt="" class="inline md:mr-2 md:h-12"></button>
+      </div>
     </div>
 
     <div class="link_mobile flex flex-row-reverse md:hidden">
@@ -26,7 +26,8 @@
       <span></span>
       <ul id="menu">
         <li><a class="hover:border-b-2 hover:border-green" href="/">{{ this.lang_fr ? "Accueil" : "Home" }} </a></li>
-        <li><a class="hover:border-b-2 hover:border-green" href="/discounts">{{ this.lang_fr ? "Chambre" : "Room" }}</a></li>
+        <!--<li><a class="hover:border-b-2 hover:border-green" href="/discounts">{{ this.lang_fr ? "Chambre" : "Room" }}</a></li>-->
+        <li><a class="hover:border-b-2 hover:border-green" href="/discounts">{{ this.lang_fr ? "Promotion" : "Discount" }}</a></li>
         <li><a class="hover:border-b-2 hover:border-green" href="/Reservation">{{ this.lang_fr ? "Reserver" : "Book" }}</a></li>
         <li><a class="hover:border-b-2 hover:border-green" href="/Register-Login">{{ this.lang_fr ? "Me Connecter" : "Login" }}</a></li>
         <li><a class="hover:border-b-2 hover:border-green" href="/Contact">{{ this.lang_fr ? "Nous contacter" : "Contact us" }}</a></li>
