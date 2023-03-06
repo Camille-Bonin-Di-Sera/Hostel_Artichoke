8adf8eb05f9ca3c103abf69855165e5b631097c8
Camille-Bonin-Di-Sera <camille.bonin@le-campus-numerique.fr>
2023-03-06 13:59:42 +0100

    little up

--- src/components/Register-Login/RegisterComponent.vue
+++ src/components/Register-Login/RegisterComponent.vue
@@ -3,7 +3,7 @@
           <div>
             <h3 class="title_count text-white text-center text-4xl sm:text-4xl md:text-6xl"> {{ this.lang_fr ? "Créer un compte" : "Create an account" }} </h3>
           </div>
-          <form class="form_resa w-full flex flex-col justify-center items-center mt-6" v-on:submit.prevent="register">
+          <form class="form_resa w-full flex flex-col justify-center items-center" v-on:submit.prevent="register">
             <input id="Pseudo" v-model="Pseudo" name="Pseudo" type="text" placeholder="Pseudo" class="border border-black w-1/2">
             <br />
             <input id="email" v-model="email" name="email" type="email" placeholder=" E-mail" class="border border-black w-1/2">
