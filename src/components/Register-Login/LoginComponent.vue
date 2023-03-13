b62ea0adc4ea293a62af731944c01207d5b95df7
Mathieu <berthetmathieu@free.fr>
2023-03-13 15:51:01 +0100

    login

--- src/components/Register-Login/LoginComponent.vue
+++ src/components/Register-Login/LoginComponent.vue
@@ -1,25 +1,25 @@
 <template>
-        <div class="v-line"></div>
-        <aside class="float-left flex flex-col justify-center items-center w-full h-max	">
-          <h3 class="title-connect text-white text-center text-4xl md:text-6xl"> {{ this.lang_fr ? "Me connecter" : "Log in" }} </h3>
-          <form class="connection md:mt-8  mt-4" v-on:submit.prevent="login">
-              <label for="emailLog"></label>
+  <div class="v-line"></div>
+  <aside class="float-left flex flex-col justify-center items-center w-full h-max	">
+    <h3 class="title-connect text-white text-center text-4xl md:text-6xl"> {{ this.lang_fr ? "Me connecter" : "Log in" }} </h3>
+    <form class="connection md:mt-8  mt-4" v-on:submit.prevent="login">
+        <label for="emailLog"></label>
 
-            <input id="emailLog" v-model="emailLog" name="email" type="email" placeholder=" E-mail" class="border border-black md:w-2/5	">
-            <br />
-              <label for="passwordLog"></label>
-            <input id="passwordLog" v-model="passwordLog" name="password" type="password" placeholder=" Mot de passe" class="border border-black md:w-2/5	">
-            <br />
-            <div class="mb-4">
-              <input type="checkbox"> <label class="text-white fontButton text-xl">{{ this.lang_fr ? "Rester connecté" : "Stay connect" }}</label>
-              <br />
-            </div>
-            <div class="fontButton">
-              <button class="text-white pr-2 pl-2 text-xl backgroundButton block m-auto w-full  rounded sm:text-3xl"> {{ this.lang_fr ? "Se connecter" : "Sign in" }} </button>
-            </div>
-          </form>
-          <br />
-        </aside>
+      <input id="emailLog" v-model="emailLog" name="email" type="email" placeholder=" E-mail" class="border border-black md:w-2/5	">
+      <br />
+        <label for="passwordLog"></label>
+      <input id="passwordLog" v-model="passwordLog" name="password" type="password" placeholder=" Mot de passe" class="border border-black md:w-2/5	">
+      <br />
+      <div class="mb-4">
+        <input type="checkbox"> <label class="text-white fontButton text-xl">{{ this.lang_fr ? "Rester connecté" : "Stay connect" }}</label>
+        <br />
+      </div>
+      <div class="fontButton">
+        <button class="text-white pr-2 pl-2 text-xl backgroundButton block m-auto w-full  rounded sm:text-3xl"> {{ this.lang_fr ? "Se connecter" : "Sign in" }} </button>
+      </div>
+    </form>
+    <br />
+  </aside>
 </template>
 
 <script>
