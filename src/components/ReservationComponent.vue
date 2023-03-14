d1f263eb91b67710c5fc77c4c9ee83fef57f0525
Mathieu <berthetmathieu@free.fr>
2023-03-14 13:14:48 +0100

    parse in reservation

--- src/components/ReservationComponent.vue
+++ src/components/ReservationComponent.vue
@@ -536,7 +536,7 @@ export default {
         price: this.priceTotalResa,
         number_invoices: this.nbInvoice++,
         fk_Reservation: this.idReservation,
-        fk_User: store.userId,
+        fk_User: parseInt(store.userId),
       })
           .then((resultat) => {
             console.log("Facture : ", resultat);
